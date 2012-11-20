aa9bca2daace999e173e63ead203eb8468ccd20f
Cyrille Faucheux <cyrille.faucheux@gmail.com>
2012-11-20 10:08:55 +0100

    Allows HaralickComputer to handle multiple offsets at once.

--- HaralickComputer.cpp
+++ HaralickComputer.cpp
@@ -84,7 +84,8 @@ class HaralickComputer : public FeaturesComputer
 private:
 	boost::program_options::options_description options;
 	unsigned int posterization_level;
-	cli_offset window, offset;
+	cli_offset window;
+	std::vector< cli_offset > offsets;
 	bool normalization;
 
 public:
@@ -97,7 +98,7 @@ public:
 			("window,w",
 				po::value< cli_offset >(&this->window)->required(), "Window radius (required)")
 			("offset,o",
-				po::value< cli_offset >(&this->offset)->required(), "Offset (required)")
+				po::value< std::vector< cli_offset > >(&this->offsets)->required()->multitoken(), "Offset (required)")
 			("normalize,n",
 			 "Enables normalization (default: disabled")
 			;
@@ -124,20 +125,33 @@ public:
 		rescaler->SetOutputMaximum(this->posterization_level - 1);
 		rescaler->Update();
 
-		// Compute the haralick features
 		typename HaralickFilter::Pointer haralickImageComputer = HaralickFilter::New();
-		typename HaralickFilter::RadiusType window_radius =
-			{{this->window[0], this->window[1], this->window[2]}};
 		haralickImageComputer->SetInput(rescaler->GetOutput());
-		haralickImageComputer->SetWindowRadius(window_radius);
 		haralickImageComputer->SetNumberOfBinsPerAxis(this->posterization_level);
 
-		typename HaralickFilter::OffsetType offset1 =
-			{{this->offset[0], this->offset[1], this->offset[2]}};
-		typename HaralickFilter::OffsetVectorType::Pointer offsetV =
-			HaralickFilter::OffsetVectorType::New();
-		offsetV->push_back(offset1);
-		haralickImageComputer->SetOffsets(offsetV);
+		{
+			typename HaralickFilter::RadiusType window_radius =
+				{{this->window[0], this->window[1], this->window[2]}};
+			haralickImageComputer->SetWindowRadius(window_radius);
+		}
+
+		{
+			typename HaralickFilter::OffsetVectorType::Pointer offsetV =
+				HaralickFilter::OffsetVectorType::New();
+			typename HaralickFilter::OffsetType offset;
+			std::vector< cli_offset >::const_iterator offsets_it;
+			for( offsets_it = this->offsets.begin(); offsets_it < this->offsets.end(); ++offsets_it)
+			{
+				offset[0] = (*offsets_it)[0];
+				offset[1] = (*offsets_it)[1];
+				offset[2] = (*offsets_it)[2];
+
+				offsetV->push_back(offset);
+			}
+
+			haralickImageComputer->SetOffsets(offsetV);
+		}
+
 		haralickImageComputer->Update();
 
 		if(!this->normalization) {
@@ -167,44 +181,6 @@ public:
 
 			return imageToVectorImageFilter->GetOutput();
 		}
-
-		//return haralickImageComputer->GetOutput();
-
-		/*
-		const typename OutputImageType::RegionType image_region = output_image->GetLargestPossibleRegion();
-		typename OutputImageType::RegionType::SizeType image_size = image_region.GetSize();
-
-		OutputImageIterator ito(output_image, image_region);
-
-		OutputImageType::PixelType out_pix;
-		OutputImageType::IndexType coords;
-		out_pix.SetSize(this->dimension);
-		ito.GoToBegin();
-		while( !ito.IsAtEnd() )
-		{
-			coords = ito.GetIndex();
-
-			out_pix[0] = coords[0];
-			out_pix[1] = coords[1];
-			if(this->dimension == 3)
-				out_pix[2] = coords[2];
-
-			if(this->normalization) {
-				out_pix[0] /= image_size[0];
-				out_pix[1] /= image_size[1];
-				if(this->dimension == 3)
-					out_pix[2] /= image_size[2];
-			}
-
-			std::cout << out_pix << std::endl;
-
-			ito.Set(out_pix);
-
-			++ito;
-		}
-		*/
-
-		//return haralickImageComputer->GetOutput();
 	}
 };
 
